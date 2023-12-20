8131346d4c227720422e60d04a596787a19adad4
Halismelih1 <hhlsabkk@icloud.com>
2023-12-21 00:33:29 +0300

    style for searchForm

--- CaseStudyForaAmadeus/src/Components/SearchForm.jsx
+++ CaseStudyForaAmadeus/src/Components/SearchForm.jsx
@@ -1,45 +1,72 @@
 import React, { useState } from 'react';
+import { Form, Input, Checkbox, DatePicker, Row, Col } from 'antd';
+
+const { RangePicker } = DatePicker;
 
 const SearchForm = () => {
-  const [departureAirport, setDepartureAirport] = useState('');
-  const [arrivalAirport, setArrivalAirport] = useState('');
+  const [form] = Form.useForm();
   const [oneWay, setOneWay] = useState(false);
 
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // istek kısmı  
+  const onFinish = (values) => {
+    console.log('Received values:', values);
+    // İstek kısmı burada yapılabilir
   };
 
-  const handleInputChange = (e) => {
-    const { name, value } = e.target;
+  const handleCheckboxChange = (e) => {
+    setOneWay(e.target.checked);
 
-
-    switch (name) {
-      case 'departureAirport':
-        setDepartureAirport(value);
-        break;
-      case 'arrivalAirport':
-        setArrivalAirport(value);
-        break;
-      default:
-        break;
+    // "Tek yönlü uçuş" seçeneği seçildiyse dönüş tarihini sıfırla
+    if (e.target.checked) {
+      form.setFieldsValue({
+        returnDate: null,
+      });
     }
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Kalkış Havaalanı:
-        <input type="text" name="departureAirport" value={departureAirport} onChange={handleInputChange} />
-      </label>
-      {/* Diğer input alanları belki */}
-      <label>
-        Tek Yönlü Uçuş:
-        <input type="checkbox" name="oneWay" checked={oneWay} onChange={() => setOneWay(!oneWay)} />
-      </label>
-      <button type="submit">Ara</button>
-    </form>
+    <div style={{ maxWidth: '60%', margin: 'auto' }}>
+      <Form form={form} onFinish={onFinish} layout="vertical">
+        <Row gutter={16}>
+          <Col xs={24} sm={12}>
+            <Form.Item label="Kalkış Havaalanı" name="departureAirport" rules={[{ required: true, message: 'Lütfen kalkış havaalanını girin.' }]}>
+              <Input />
+            </Form.Item>
+          </Col>
+          <Col xs={24} sm={12}>
+            <Form.Item label="Varış Havaalanı" name="arrivalAirport" rules={[{ required: true, message: 'Lütfen varış havaalanını girin.' }]}>
+              <Input />
+            </Form.Item>
+          </Col>
+        </Row>
+        <Row>
+          <Col span={24}>
+            <Form.Item label="Uçuş Tipi">
+              <Checkbox name="oneWay" checked={oneWay} onChange={handleCheckboxChange}>
+                Tek Yön
+              </Checkbox>
+              <Checkbox name="roundTrip" checked={!oneWay} onChange={() => setOneWay(!oneWay)}>
+                Gidiş-Dönüş
+              </Checkbox>
+            </Form.Item>
+          </Col>
+        </Row>
+        <Row gutter={16}>
+          <Col xs={24} sm={12}>
+            <Form.Item label="Gidiş Tarihi" name="departureDate" rules={[{ required: true, message: 'Lütfen gidiş tarihini girin.' }]}>
+              <DatePicker showTime={false} format="YYYY-MM-DD" />
+            </Form.Item>
+          </Col>
+          {!oneWay && (
+            <Col xs={24} sm={12}>
+              <Form.Item label="Dönüş Tarihi" name="returnDate" rules={[{ required: true, message: 'Lütfen dönüş tarihini girin.' }]}>
+                <DatePicker showTime={false} format="YYYY-MM-DD" />
+              </Form.Item>
+            </Col>
+          )}
+        </Row>
+      </Form>
+     
+    </div>
   );
 };
 
